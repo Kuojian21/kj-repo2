f55aea5ed817e2c3a71cca37f1cadbda7059e6fe
Kuojian21 <Kuojian21@163.com>
2018-10-31 01:02:22 +0800

    tj

--- kj-repo-hystrix/src/main/java/com/kj/repo/hystrix/rx/RxJavaTest.java
+++ kj-repo-hystrix/src/main/java/com/kj/repo/hystrix/rx/RxJavaTest.java
@@ -1,36 +1,89 @@
 package com.kj.repo.hystrix.rx;
 
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.collect.Lists;
+
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
 
 public class RxJavaTest {
 
-	public static void main(String[] args) {
-
-		Observable.create(t -> {
-			t.onNext("kj");
-		}).map(t -> t + "map").subscribe(new Subscriber<String>() {
+	public static <T> Observer<T> observer(){
+		return new Observer<T>() {
 
 			@Override
 			public void onCompleted() {
 				// TODO Auto-generated method stub
-				System.out.println("kj finish");
+				System.out.println("finish");
 			}
 
 			@Override
 			public void onError(Throwable e) {
 				// TODO Auto-generated method stub
-				e.printStackTrace();
+				System.out.println("exception " + e.getMessage());
 			}
 
 			@Override
-			public void onNext(String t) {
+			public void onNext(T t) {
 				// TODO Auto-generated method stub
-				System.out.println(t);
+				System.out.println("next " + t);
 			}
-
-		}).unsubscribe();
-
+			
+		};
 	}
+	
+	public static void create() {
+		Observer<String> observer = observer();
+		Observable.create(t -> {
+			t.onNext("kj");
+		}).map(t -> t + "map").subscribe(observer).unsubscribe();
+	}
+	
+	public static void range() {
+		Observer<Integer> observer = observer();
+		Observable.range(2, 10).subscribe(observer);
+	}
+	
+	public static void just() {
+		Observer<Integer> observer = observer();
+		Observable.just(1).subscribe(observer);
+	}
+	
+	public static void timer() {
+		Observer<Long> observer = observer();
+		Observable.timer(5, TimeUnit.SECONDS).subscribe(observer);
+	}
+	
+	public static void interval() {
+		Observer<Long> observer = observer();
+		Observable.interval(5, TimeUnit.SECONDS).subscribe(observer);
+	}
+	
+	public static void defer() {
+		Observer<Integer> observer = observer();
+		Observable.defer(()->Observable.from(Lists.newArrayList(3,6,9))).subscribe(observer);
+		
+	}
+	
+	public static void buffer() {
+		
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+//		create();
+//		range();
+//		just();
+		timer();
+		interval();
+		defer();
+		
+		
+		Thread.sleep(10000);
+	
+	}
+	
+	
 
 }
