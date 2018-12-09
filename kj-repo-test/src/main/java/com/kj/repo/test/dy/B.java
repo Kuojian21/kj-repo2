d1841caeafe20196ac390ea14602f62a7de74e2a
Kuojian21 <Kuojian21@163.com>
2018-12-09 22:01:35 +0800

    tj

---
+++ kj-repo-test/src/main/java/com/kj/repo/test/dy/B.java
@@ -0,0 +1,37 @@
+package com.kj.repo.test.dy;
+
+import com.google.common.base.Function;
+import com.google.common.base.Supplier;
+
+class A {
+	static void foo(Runnable runnable) {
+		System.out.println("A.foo()");
+		runnable.run();
+	}
+
+	static <T> T foo(Supplier<T> supplier) {
+		System.out.println("A.foo() NEW");
+		return supplier.get();
+	}
+	
+	static <T> T foo(Function<String,T> fun) {
+		System.out.println("A.foo() NEW");
+		return fun.apply("a");
+	}
+}
+
+public class B {
+	public static void main(String[] args) {
+		A.foo(B::myTest);
+	}
+
+	private static String myTest() {
+		System.out.println("B.myTest() 1");
+		return "test";
+	}
+
+	private static String myTest(Object obj) {
+		System.out.println("B.myTest() 2");
+		return "test";
+	}
+}
---
+++ kj-repo-test/src/main/java/com/kj/repo/test/io/IOTest.java
@@ -0,0 +1,11 @@
+package com.kj.repo.test.io;
+
+import java.io.IOException;
+
+public class IOTest {
+
+	public static void main(String[] args) throws IOException {
+		System.in.read();
+	}
+
+}
